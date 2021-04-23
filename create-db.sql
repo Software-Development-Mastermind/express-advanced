8e6a8cb50fd91cfa1da6589c72b5bc0061a7e131
Andy <andysterks@users.noreply.github.com>
2021-04-23 09:47:15 -0700

    Updates create-db.sql file

--- create-db.sql
+++ create-db.sql
@@ -4,8 +4,7 @@ BEGIN
     IF EXISTS (SELECT FROM pg_database WHERE datname = 'full_stack_todos') THEN
       RAISE NOTICE 'Database already exists'; 
     ELSE
-        PERFORM dblink_exec('dbname=' || current_database()  -- current db
-                            , 'CREATE DATABASE full_stack_todos');
+      CREATE DATABASE full_stack_todos;
     END IF;
 END
 $do$;
